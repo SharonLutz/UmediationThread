aa47c95a83ceef2921b8a903a28093f4128a59a7
Michael Gooch <michael_gooch@harvardpilgrim.org>
2019-07-29 20:59:33 -0400

    tried to wrap functions in try/catch to see if it would allow me to prevent rstudio from crashing alongside the interpreter.
    it didn't work, so either the crash is not catchable or it is happening in the threads, and i'll have to wrap them in try/catch and have them gracefully terminate.

--- src/mediation_helper_functions.cpp
+++ src/mediation_helper_functions.cpp
@@ -5,33 +5,59 @@
 #endif
 #include <Rcpp.h>
 #include <RcppEigen.h>
+#include <exception>
+#include <sstream>
 #include  "mediation_helper.hpp"
 
 // [[Rcpp::export]]
 void mediate_helper(Rcpp::Environment &env){
   // Eigen::setNbThreads(1);
-  MediationHelper h(env);
-  h();
+  try{
+    MediationHelper h(env);
+    h();
+  }catch(std::exception &e){
+    std::stringstream ss;
+    ss << "error in mediate_helper, exception caught: " << e.what() << std::endl;
+    Rf_error(ss.str().c_str());
+  }catch(...){
+    Rf_error("error in mediate_helper: Unknown Exception type caught!");
+  }
   // Eigen::setNbThreads(0);
 }
 
 // [[Rcpp::export]]
 void mediate_helper_variable_exporter(Rcpp::Environment &env){
   // Eigen::setNbThreads(1);
-  MediationHelper h(env, true);
-  h();
+  try{
+    MediationHelper h(env, true);
+    h();
+  }catch(std::exception &e){
+    std::stringstream ss;
+    ss << "error in mediate_helper_variable_exporter, exception caught: " << e.what() << std::endl;
+    Rf_error(ss.str().c_str());
+  }catch(...){
+    Rf_error("error in mediate_helper_variable_exporter: Unknown Exception type caught!");
+  }
   // Eigen::setNbThreads(0);
 }
 
 // [[Rcpp::export]]
 void threaded_mediate_helper(Rcpp::Environment &env, long long int num_threads){
   // Eigen::setNbThreads(1);
-  if(num_threads > 1){
-    Eigen::initParallel();
-    MediationHelper h(env, num_threads);
-    h();
-  } else {
-    mediate_helper(env);
+  try{
+    if(num_threads > 1){
+      Eigen::initParallel();
+      MediationHelper h(env, num_threads);
+      h();
+    } else {
+      mediate_helper(env);
+    }
+  }catch(std::exception &e){
+    std::stringstream ss;
+    ss << "error in threaded_mediate_helper, exception caught: " << e.what() << std::endl;
+    Rf_error(ss.str().c_str());
+  }catch(...){
+    Rf_error("error in threaded_mediate_helper: Unknown Exception type caught!");
   }
   // Eigen::setNbThreads(0);
 }
